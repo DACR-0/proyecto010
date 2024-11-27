86bde9f783d28c8962d1d4dcffa3e2c5d14a76c0
DACR-0 <dacr2312@gmail.com>
2024-11-27 09:15:12 -0500

    avance 2 del proyecto

--- src/app/api/ec_d_e/route.js
+++ src/app/api/ec_d_e/route.js
@@ -2,7 +2,7 @@ import { pool } from '@/utils/db';
 
 export async function GET(request) {
   try {
-    const [rows] = await pool.query('SELECT id_fa, nombre_cargo, porcentaje FROM f_administrativas ORDER BY nombre_cargo ASC');
+    const [rows] = await pool.query('SELECT nombre_fe,porcentaje_max_fe FROM proyecto010.f_extencion ORDER BY nombre_fe ASC');
     return new Response(JSON.stringify(rows), { status: 200 });
   } catch (error) {
     console.error('Error al obtener los cargos:', error);
--- src/app/api/ec_d_i/route.js
+++ src/app/api/ec_d_i/route.js
@@ -2,7 +2,7 @@ import { pool } from '@/utils/db';
 
 export async function GET(request) {
   try {
-    const [rows] = await pool.query('SELECT id_fa, nombre_cargo, porcentaje FROM f_administrativas ORDER BY nombre_cargo ASC');
+    const [rows] = await pool.query('SELECT nombre_fi, porcentaje_max_fi FROM proyecto010.f_investigacion ORDER BY nombre_fi ASC');
     return new Response(JSON.stringify(rows), { status: 200 });
   } catch (error) {
     console.error('Error al obtener los cargos:', error);
